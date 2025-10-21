tryemerge/vibe-factory

wip: server implementation and basic transport

crates/remote/src/db/mod.rs
@@ -1,0 +1,20 @@
+pub mod activity;
+pub mod listener;
+pub mod organizations;
+pub mod tasks;
+
+pub use listener::ActivityListener;
+use sqlx::{PgPool, Postgres, Transaction, migrate::MigrateError, postgres::PgPoolOptions};
+
+pub(crate) type Tx<'a> = Transaction<'a, Postgres>;
+
+pub(crate) async fn migrate(pool: &PgPool) -> Result<(), MigrateError> {
+    sqlx::migrate!("./migrations").run(pool).await
+}
+
+pub(crate) async fn create_pool(database_url: &str) -> Result<PgPool, sqlx::Error> {
+    PgPoolOptions::new()
+        .max_connections(10)
+        .connect(database_url)
+        .await
+}

skcc-leejunhyoung/vibe-kanban

crates/remote/src/db/mod.rs
@@ -1,0 +1,21 @@
+pub mod activity;
+pub mod identity;
+pub mod listener;
+pub mod projects;
+pub mod tasks;
+
+pub use listener::ActivityListener;
+use sqlx::{PgPool, Postgres, Transaction, migrate::MigrateError, postgres::PgPoolOptions};
+
+pub(crate) type Tx<'a> = Transaction<'a, Postgres>;
+
+pub(crate) async fn migrate(pool: &PgPool) -> Result<(), MigrateError> {
+    sqlx::migrate!("./migrations").run(pool).await
+}
+
+pub(crate) async fn create_pool(database_url: &str) -> Result<PgPool, sqlx::Error> {
+    PgPoolOptions::new()
+        .max_connections(10)
+        .connect(database_url)
+        .await
+}