9c8145c9c00e627c5b7334d41d6228162027701a
Louis Knight-Webb <louis@bloop.ai>
2026-02-21 18:33:27 +0000

    refactor(ui): extract AutoResizeTextarea to @vibe/ui package

--- packages/ui/src/components/AutoResizeTextarea.tsx
+++ packages/ui/src/components/AutoResizeTextarea.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { cn } from '@/lib/utils';
+import { cn } from '../lib/cn';
 
 function normalizeSingleLineValue(value: string): string {
   return value.replace(/\r\n|\r|\n/g, ' ');
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,3 +1,4 @@
 export { cn } from './lib/cn';
+export { AutoResizeTextarea } from './components/AutoResizeTextarea';
 export { RunningDots } from './components/RunningDots';
 export { StatusDot } from './components/StatusDot';
