8252fb9bb7052e277c6a391e26547e831912da64
Junhyoung <junhyoung@skcc.com>
2026-08-11 23:12:30 +0900

    fix(workspaces): 모바일 채팅 Esc 단계 처리 (#5372)

--- packages/web-core/src/pages/workspaces/WorkspacesLayout.tsx
+++ packages/web-core/src/pages/workspaces/WorkspacesLayout.tsx
@@ -325,7 +325,6 @@ export function WorkspacesLayout({
 
             {/* Chat tab */}
             <div
-              data-escape-handled
               className={cn(
                 'flex-1 min-h-0 overflow-hidden',
                 mobileTab !== 'chat' && 'hidden'
